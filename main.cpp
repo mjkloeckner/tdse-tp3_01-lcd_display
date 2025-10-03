4e28b70ad50d90234dce948b20952d20d3bc9650
Martin Klöckner <mjkloeckner@gmail.com>
2025-10-03 21:11:56 +0000

    First commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 //=====[Libraries]=============================================================
 
+#include "mbed.h"
 #include "smart_home_system.h"
 
 //=====[Main function, the program entry point after power on or reset]========
 
 int main()
 {
+    printf("Hello, World!\n");
     smartHomeSystemInit();
     while (true) {
         smartHomeSystemUpdate();
